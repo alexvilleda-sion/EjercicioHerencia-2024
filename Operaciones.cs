81c86eb31f43f888616f0f306cd93b13f5a60645
alexvilleda-sion <100446107+alexvilleda-sion@users.noreply.github.com>
2024-10-23 21:03:56 -0600

    Add files via upload

---
+++ Operaciones.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EjercicioHerencia
+{
+    // Clase padre
+    public class Operaciones
+    {
+        // Método para calcular el área de un rectángulo
+        public double CalcularAreaRectangulo(double largo, double ancho)
+        {
+            return largo * ancho;
+        }
+
+        // Método para calcular la circunferencia de un círculo
+        public double CalcularCircunferencia(double radio)
+        {
+            return 2 * Math.PI * radio;
+        }
+
+        // Método para calcular el volumen de un cubo
+        public double CalcularVolumenCubo(double lado)
+        {
+            return Math.Pow(lado, 3);
+        }
+    }
+}
